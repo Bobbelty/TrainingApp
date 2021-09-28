743766f5644beec33d6aef54fa6b14f18d624719
Valle20 <valdemar433@gmail.com>
2021-09-28 09:45:29 +0200

    Adding all tabs to the bottom navigation menu.

--- app/src/main/java/com/example/trainingapp/ui/schedule/ScheduleViewModel.java
+++ app/src/main/java/com/example/trainingapp/ui/schedule/ScheduleViewModel.java
@@ -1,14 +1,14 @@
-package com.example.trainingapp.ui.dashboard;
+package com.example.trainingapp.ui.schedule;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ScheduleViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ScheduleViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
