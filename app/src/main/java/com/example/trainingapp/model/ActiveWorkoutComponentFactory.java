c51e30f75cd600c831b2639b13739d8ca9a13cd5
oswalchlm <waoscar@student.chalmers.se>
2021-10-14 13:52:11 +0200

    Class setup
    Co-Authored-By: Valle20 <73998879+Valle20@users.noreply.github.com>
    Co-Authored-By: vichui1 <74008544+vichui1@users.noreply.github.com>

---
+++ app/src/main/java/com/example/trainingapp/model/ActiveWorkoutComponentFactory.java
@@ -0,0 +1,12 @@
+package com.example.trainingapp.model;
+
+
+
+/**
+ * Class providing static methods for creating resources
+ */
+public class ActiveWorkoutComponentFactory {
+
+
+
+}
---
+++ app/src/main/java/com/example/trainingapp/model/ActiveWorkoutSession.java
@@ -0,0 +1,12 @@
+package com.example.trainingapp.model;
+
+import java.util.Calendar;
+
+/**
+ * Class handling logic for ActiveExercise and ActiveWorkout
+ */
+public class ActiveWorkoutSession {
+
+
+
+}
