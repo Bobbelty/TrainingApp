ac7d54b662bb5792d3341532f4730990ce25b025
oswalchlm <waoscar@student.chalmers.se>
2021-10-17 16:20:04 +0200

    Initial classes
    Co-Authored-By: Valle20 <73998879+Valle20@users.noreply.github.com>
    Co-Authored-By: vichui1 <74008544+vichui1@users.noreply.github.com>

---
+++ app/src/main/java/com/example/trainingapp/view/Adapter/HistoryRecyclerViewAdapter.java
@@ -0,0 +1,22 @@
+package com.example.trainingapp.view.Adapter;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.trainingapp.R;
+
+public class HistoryRecyclerViewAdapter extends RecyclerView.Adapter<EditScheduleRecyclerViewAdapter.ListViewHolder>{
+
+
+    static class ListViewHolder extends RecyclerView.ViewHolder{
+
+        public ListViewHolder(@NonNull View itemView) {
+
+        }
+    }
+
+}
